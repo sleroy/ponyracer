24b3259d32ebaf4dd79623d63d02e4a278415979
Xubuntu <sleroy0@gmail.com>
2017-01-18 16:18:42 +0100

    ex 10

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('ponyracer App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Ponyracer');
+    expect(page.getParagraphText()).toEqual('Ponyracer Always a pleasure to bet on ponies');
   });
 });
