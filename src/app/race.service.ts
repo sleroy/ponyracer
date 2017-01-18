c98a4c680377a7ac352adad18cbfa987409484c7
Xubuntu <sleroy0@gmail.com>
2017-01-18 14:17:55 +0100

    exercice 7

--- src/app/race.service.ts
+++ src/app/race.service.ts
@@ -1,40 +1,25 @@
 import { Injectable } from '@angular/core';
 import { RaceModel } from './models/race.model';
 import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class RaceService {
+ private http: Http;
 
-  constructor() { }
+  public apiUrl: string = 'http://ponyracer.ninja-squad.com';
+
+  constructor(http: Http) {
+    this.http = http;
+
+  }
 
   /** Returns a list of services */
   list(): Observable<Array<RaceModel>> {
-    return Observable.of([
-      {
-        id: 12,
-        name: 'Paris',
-        ponies: [
-          { id: 1, name: 'Gentle Pie', color: 'YELLOW' },
-          { id: 2, name: 'Big Soda', color: 'ORANGE' },
-          { id: 3, name: 'Gentle Bottle', color: 'PURPLE' },
-          { id: 4, name: 'Superb Whiskey', color: 'GREEN' },
-          { id: 5, name: 'Fast Rainbow', color: 'BLUE' }
-        ],
-        startInstant: '2016-02-18T08:02:00Z'
-      }, {
-        id: 13,
-        name: 'Tokyo',
-        ponies: [
-          { id: 6, name: 'Fast Rainbow', color: 'BLUE' },
-          { id: 7, name: 'Gentle Castle', color: 'GREEN' },
-          { id: 8, name: 'Awesome Rock', color: 'PURPLE' },
-          { id: 9, name: 'Little Rainbow', color: 'YELLOW' },
-          { id: 10, name: 'Great Soda', color: 'ORANGE' }
-        ],
-        startInstant: '2016-02-18T08:03:00Z'
-      }
-    ]);
+    // Conversion of Response into RaceModel
+    return this.http.get(`${this.apiUrl}/api/races?status=PENDING`).map(response => response.json());
   }
 
 }
