1762f41b26c5d58242d6465b91cf469a421de7da
Xubuntu <sleroy0@gmail.com>
2017-01-18 00:52:10 -0800

    Exercice 3 resources

---
+++ src/app/race.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { RaceModel } from './models/race.model';
+
+@Injectable()
+export class RaceService {
+
+  constructor() { }
+
+  /** Returns a list of services */
+  list(): Array<RaceModel> {
+    return [
+      { name: 'Lyon' }, {
+        name: 'London'
+      }
+    ];
+  }
+
+}
