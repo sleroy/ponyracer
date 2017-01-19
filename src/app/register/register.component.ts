52529ee6a7814dffa5ad6f2cd7c0e536d722ea4f
Xubuntu <sleroy0@gmail.com>
2017-01-19 12:04:50 +0100

    form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Component, OnInit, Injectable } from '@angular/core';
 
 @Component({
@@ -8,23 +8,44 @@ import { Component, OnInit, Injectable } from '@angular/core';
 })
 @Injectable()
 export class RegisterComponent implements OnInit {
-  userForm: FormGroup;
+  userForm: FormGroup;
 
   loginCtrl: FormControl;
   passwordCtrl: FormControl;
+  confirmPasswordCtrl: FormControl;
   birthYearCtrl: FormControl;
+  passwordForm: FormGroup;
+
+
+  static passwordMatch(passwordForm: FormGroup) {
+    console.log(`Password ${passwordForm.get('password').value} ${passwordForm.get('confirmPassword').value}`);
+    return passwordForm.get('password').value !== passwordForm.get('confirmPassword').value ? {
+      matchingError: true
+    } : null;
+  }
 
   constructor(private fb: FormBuilder) {
-    this.loginCtrl = fb.control('');
-    this.passwordCtrl = fb.control('');
-    this.birthYearCtrl = fb.control('1983');
+    this.loginCtrl = fb.control('', [Validators.required, Validators.minLength(3)]);
+
+    this.passwordCtrl = fb.control('', [Validators.required]);
+    this.confirmPasswordCtrl = fb.control('', [Validators.required]);
+    this.birthYearCtrl = fb.control('1983', [Validators.required]);
+
+    this.passwordForm = fb.group(
+      { password: this.passwordCtrl, confirmPassword: this.confirmPasswordCtrl },
+      { validator: RegisterComponent.passwordMatch });
+
+
+
 
     this.userForm = fb.group({
       login: this.loginCtrl,
-      password: this.passwordCtrl,
+      passwordForm: this.passwordForm,
       birthYear: this.birthYearCtrl
     });
   }
+
+
 
   ngOnInit() {
 
